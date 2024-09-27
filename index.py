f31f0d4068f6e1d03690f0f002ca6fc31a035e58
Giovani-Mikovski-IFPR <giovani.mikovski.ifpr@gmail.com>
2024-09-27 16:01:19 -0300

    Abrir e pesquisar

---
+++ index.py
@@ -0,0 +1,24 @@
+"""
+PASSO 01: acessar o edge
+PASSO 02: digitar e enter (10 vezes)
+PASSO 03: 
+PASSO 04: 
+PASSO 05:
+PASSO 06:
+"""
+
+import pyautogui #Controla mouse e teclado
+import time #Controla o tempo
+
+pyautogui.PAUSE=0.5 #Espera o sistema iniciar
+
+'PASSO 1: acessar o edge'
+pyautogui.press("win")
+pyautogui.write("edge")
+pyautogui.press("enter")
+
+'PASSO 2: digitar e enter'
+for i in range(10):
+    pyautogui.write("abc")
+    pyautogui.press("enter")
+    time.sleep(3)
