769b7dd3be7c960e033154f2b818e605f0f6099f
Giovani-Mikovski <giovani.mikovski@gmail.com>
2024-09-27 17:30:12 -0300

    Fazer todas as tarefas

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ PASSO 01: acessar o edge
 PASSO 02: digitar e enter (10 vezes)
 PASSO 03: clicar na "medalha" e em "exibr painel"
 PASSO 04: fazer conjunto diário
-PASSO 05:
+PASSO 05: rolar e fazer mais atividades
 PASSO 06:
 """
 
@@ -30,21 +30,60 @@ time.sleep(3)
 
 pyautogui.click(x=1777, y=208) #Clicar em "Exibir painel"
 time.sleep(10)
-'''
+
 'PASSO 04: fazer conjunto diário'
 time.sleep(10)
 
 pyautogui.click(x=563, y=912) #Clica no quadrado 1
 time.sleep(5)
-pyautogui.hotkey('ctrl','w') # Fecha a guia
+pyautogui.hotkey("ctrl","w") # Fecha a guia
 time.sleep(3)
 
 pyautogui.click(x=1163, y=936) #Clica no quadrado 2
 time.sleep(5)
-pyautogui.hotkey('ctrl','w') # Fecha a guia
+pyautogui.hotkey("ctrl","w") # Fecha a guia
 time.sleep(3)
 
 pyautogui.click(x=1593, y=923) #Clica no quadrado 3
 time.sleep(5)
-pyautogui.hotkey('ctrl','w') # Fecha a guia
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+'''
+'PASSO 05: rolar e fazer mais atividades'
+time.sleep(10)
+pyautogui.scroll(-1100) # Rola para chegar em mais atividades
+
+pyautogui.click(x=304, y=361) #Clica no quadrado 0 0
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+
+pyautogui.click(x=744, y=410) #Clica no quadrado 0 1
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+
+pyautogui.click(x=1169, y=305) #Clica no quadrado 0 2
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+
+pyautogui.click(x=289, y=795) #Clica no quadrado 1 0
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+
+pyautogui.click(x=714, y=818) #Clica no quadrado 1 1
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+
+pyautogui.click(x=1155, y=780) #Clica no quadrado 1 2
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
+time.sleep(3)
+
+pyautogui.click(x=1568, y=791) #Clica no quadrado 1 3
+time.sleep(5)
+pyautogui.hotkey("ctrl","w") # Fecha a guia
 time.sleep(3)
---
+++ pegar_posicao(mais_atividades).py
@@ -0,0 +1,16 @@
+import time
+import pyautogui
+
+time.sleep(5)
+pyautogui.scroll(-1100)
+time.sleep(5)
+print(pyautogui.position())
+
+"QUADRADO 0 0: Point(x=304, y=361)"
+"QUADRADO 0 1: Point(x=744, y=410)"
+"QUADRADO 0 2: Point(x=1169, y=305)"
+"QUADRADO 1 0: Point(x=289, y=795)"
+"QUADRADO 1 1: Point(x=714, y=818)"
+"QUADRADO 1 2: Point(x=1155, y=780)"
+"QUADRADO 1 3: Point(x=1568, y=791)"
+
