1f8425731b41160748719a04e2ea1cd5cfd7ffe0
Giovani-Mikovski <giovani.mikovski@gmail.com>
2024-10-03 14:19:14 -0300

    Correção gramatical

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 
 """
+CÓDIGO DE PESQUISA NO EDGE
+
 PASSO 01: acessar o edge
-PASSO 02: pesquisa - digitar e enter (10 vezes)
+PASSO 02: pesquisar - digitar e enter (10 vezes)
 PASSO 03: clicar na "medalha" e em "exibr painel"
 PASSO 04: fazer conjunto diário
 PASSO 05: rolar e fazer mais atividades
@@ -9,6 +11,7 @@ PASSO 06: fechar edge
 PASSO 07: exibir notificação na tela
 """
 
+"Bibliotecas"
 import pyautogui #Controla mouse e teclado
 import time #Controla o tempo
 import pandas as pd #Lê a tabela de termos de pesquisa
@@ -22,28 +25,28 @@ pyautogui.press("win")
 pyautogui.write("edge")
 pyautogui.press("enter")
 
-'PASSO 2: pesquisa - digitar e enter (10 vezes)'
+'PASSO 2: pesquisar - digitar e enter (10 vezes)'
 tabela_termos=pd.read_csv("termos_pesquisa.csv") #Lê os termos
 
 for i in range(10):
-    posicao_termo=random.randint(1,49) #Gera um número aleatório entre 1 e 5 que será utilzado como index
+    posicao_termo=random.randint(1,49) #Gera um número aleatório entre 1 e 50 que será utilzado como index
     termo=tabela_termos.loc[posicao_termo,"termos"] #Pega o termo do index definido acima
     pyautogui.write(str(termo)) #Escreve na pesquisa do edge
     pyautogui.press("enter")
     time.sleep(5)
 
 'PASSO 03: clicar na "medalha" e em "exibr painel"'
-pyautogui.click(x=1745, y=146) #Clicar na "medalha"
+pyautogui.click(x=1745, y=146) #Clica na "medalha"
 time.sleep(3)
 
-pyautogui.click(x=1777, y=208) #Clicar em "Exibir painel"
+pyautogui.click(x=1777, y=208) #Clica em "Exibir painel"
 
 'PASSO 04: fazer conjunto diário'
 time.sleep(10)
 def conjunto_diario(px, py):
     pyautogui.click(px, py) #Clica no quadrado
     time.sleep(5)
-    pyautogui.hotkey("ctrl","w") # Fecha a guia
+    pyautogui.hotkey("ctrl","w") #Fecha a guia
     time.sleep(3)
 
 conjunto_diario(563,912) #Clica no quadrado 1
@@ -52,12 +55,12 @@ conjunto_diario(1593,923) #Clica no quadrado 3
 
 'PASSO 05: rolar e fazer mais atividades'
 time.sleep(5)
-pyautogui.scroll(-1100) # Rola para chegar em mais atividades
+pyautogui.scroll(-1100) #Rola para chegar em mais atividades
 
 def mais_atividades(px, py):
     pyautogui.click(px, py) #Clica no quadrado
     time.sleep(5)
-    pyautogui.hotkey("ctrl","w") # Fecha a guia
+    pyautogui.hotkey("ctrl","w") #Fecha a guia
     time.sleep(3)
 
 mais_atividades(304,361) #Clica no quadrado 0 0
@@ -73,6 +76,6 @@ mais_atividades(1568,791) #Clica no quadrado 1 3
 pyautogui.hotkey("alt","f4") #Fecha o edge
 
 'PASSO 07: exibir notificação na tela'
-noticacao=Notification(app_id="Pesquisa no edge", title="Código de pesquisa no edge concluído", msg="Pesquisa automática para pontuação no edge executada com sucesso", duration="long", icon=r"C:\Users\giova\OneDrive\Área de Trabalho\Programação\Codigo_edge\img\icon_py.png") #Descrição das características das notificações
+noticacao=Notification(app_id="Pesquisa no edge automática", title="Código de pesquisa no edge concluído", msg="Pesquisa automática para pontuação no edge executada com sucesso", duration="long", icon=r"C:\Users\giova\OneDrive\Área de Trabalho\Programação\Codigo_edge\img\icon_py.png") #Descrição das características das notificações
 
 noticacao.show() #Mostra a notificação
