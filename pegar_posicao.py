7305f8895c364b3dad3f438771865be0ddf5bbe9
Giovani-Mikovski <giovani.mikovski.ifpr@gmail.com>
2025-04-18 14:00:22 -0300

    Excusao de arquivos inuteis

--- pegar_posicao.py
+++ pegar_posicao.py
@@ -11,20 +11,3 @@ print(pyautogui.position())
 "PRIMEIRO QUADRADO: Point(x=424, y=1023)"
 "SEGUNDO QUADRADO: Point(x=1163, y=1009)"
 "TERCEIRO QUADRADO: Point(x=1603, y=1012)"
-
-"PARTE 2"
-"""
-Meus jogos: Point(x=287, y=440)
-Start: Point(x=502, y=397)
-
-Rewards: Point(x=750, y=183)
-
-Bônus de check-in diário
-Point(x=722, y=360)
-Point(x=800, y=361)
-Point(x=880, y=356)
-Point(x=956, y=362)
-Point(x=1034, y=358)
-Point(x=1111, y=361)
-Point(x=1199, y=360)
-"""
